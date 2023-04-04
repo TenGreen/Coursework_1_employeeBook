e83549d69eb70185017f3bf6aed27fdba2668832
TenGreen <Grishintut@yandex.ru>
2023-04-04 11:51:15 +0300

    Кое-что уже сделал. Сразу с заходом на повышенную сложность

---
+++ src/Employee.java
@@ -0,0 +1,54 @@
+import java.lang.invoke.StringConcatFactory;
+public class Employee {
+    private String lastName;
+    private String firstName;
+    private String patronymic;
+    private int department;
+    private double salary;
+    private static int id = 0;
+    public Employee (String lastName, String firstName,
+                    String patronymic,
+                    int department, double salary) {
+        id ++;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.patronymic = patronymic;
+        this.department = department;
+        this.salary = salary;
+        this.id = id;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getPatronymic() {
+        return patronymic;
+    }
+    public int getDepartment() {
+        return department;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setFirstName(String ConcatFactory) {
+        this.firstName = firstName;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        EmployeeBook employeeBook = new EmployeeBook();
+        int i = 0;
+        int j = 0;
+        employeeBook.printStartMenu();
+
+
+        while (true) {
+            System.out.println("Что вы хотите сделать?");
+            System.out.println("1. Получить список всех сотрудников " +
+                    "со всеми имеющимися по ним данными");
+            System.out.println("2. Посчитать сумму затрат на зарплаты в месяц.");
+            System.out.println("3. Найти сотрудника с минимальной зарплатой.");
+            System.out.println("4. Найти сотрудника с максимальной зарплатой.");
+            System.out.println("5. Подсчитать среднее значение зарплат.");
+            System.out.println("6. Получить Ф. И. О. всех сотрудников (вывести в консоль).");
+            Scanner
+        }
+/*1. Получить список всех сотрудников со всеми имеющимися
+        * по ним данными (вывести в консоль значения всех полей (toString)).
+        2. Посчитать сумму затрат на зарплаты в месяц.
+        3. Найти сотрудника с минимальной зарплатой.
+        4. Найти сотрудника с максимальной зарплатой.
+        5. Подсчитать среднее значение зарплат (можно использовать
+        * для этого метод из пункта b).
+        6. Получить Ф. И. О. всех сотрудников (вывести в консоль).*/
     }
 }
